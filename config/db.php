b8d4ee261de8becdde77be63807311bae28a46f2
Sergey Ponomarev <shtiher@perfectpanel.com>
2022-02-01 14:56:55 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=test_db',
+    'username' => 'root',
+    'password' => 'my_root_pwd',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
